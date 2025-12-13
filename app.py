4c8abaeb416a0c59e530f4c461e8b43212ea0833
olatunde <tundehol4luv@gmail.com>
2025-12-13 17:13:32 +0100

    web app is ready

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def home():
     return render_template('home.html')
 
 @app.route('/predict_api', methods=['POST'])
-def predict():
+def predict_api():
     data = request.json['data']
     print(data)
     print(np.array(list[data.values()]).reshape(1,-1))
@@ -22,6 +22,15 @@ def predict():
     output = model.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = model.predict(final_input)[0]
+    return render_template('home.html', prediction_text='The predicted house price is $ {}'.format(output))
 
 
 
